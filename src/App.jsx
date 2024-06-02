cbaea7c8c91d3271ca7baccae9db23b557a36d69
jorjishasan <dev.jorjis@gmail.com>
2024-06-02 19:41:47 +0600

    refactor: Update Accordion component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { accordionData } from "./constant";
+import AccordionFrame from "./AccordionFrame";
+import caret from "./assets/caret-down.svg";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Accordion = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+
+  // const [activeIndex, setActiveIndex] = useState(null);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="border-rounded font-mono shadow-custom-blue rounded-2xl flex items-center flex-col">
+      <div
+        onClick={handleClick}
+        className={`w-full select-none cursor-pointer flex gap-[24px] justify-center py-8 ${
+          isOpen && "border-b-[1px] border-dashed border-gray-800"
+        }`}
+      >
+        <h1 className="text-[32px]">Container</h1>
+        <img className={isOpen && "rotate-180"} src={caret} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {isOpen && (
+        <div className="flex flex-col gap-3 w-[500px] mb-24 mt-[48px] ">
+          {accordionData.map((item, index) => (
+            <AccordionFrame key={index} item={item} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default Accordion;
