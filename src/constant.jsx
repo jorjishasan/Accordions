db0563ed80c5893dfe2c2e5497a2318ddc7d85e0
jorjishasan <dev.jorjis@gmail.com>
2024-06-02 19:41:05 +0600

    refactor: Add accordionData constant to constant.jsx

---
+++ src/constant.jsx
@@ -0,0 +1,19 @@
+export const accordionData = [
+  {
+    heading: "Heading One",
+    content: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+  },
+  {
+    heading: "Heading Two",
+    content:
+      "Lorem ipsum dolor sit amet consectetur. Vulputate volutpat gravida fringilla at fermentum purus eget augue consequat. Vivamus sit magnis dui ipsum tortor dui quam. Mauris consectetur praesent tempus est nunc vel sem quis molestie. Donec sed eget tristique.",
+  },
+  {
+    heading: "Heading Three",
+    content: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+  },
+  {
+    heading: "Heading Four",
+    content: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+  },
+];
