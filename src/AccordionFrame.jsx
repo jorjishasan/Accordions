1880c5cc8acf3ff77985740d3507328e5c2356ec
jorjishasan <dev.jorjis@gmail.com>
2024-06-04 03:47:51 +0600

    Add: accordion frame component

--- src/AccordionFrame.jsx
+++ src/AccordionFrame.jsx
@@ -1,21 +1,23 @@
-import { useState } from "react";
+/* eslint-disable react/prop-types */
 import aeroDown from "./assets/aero_down.svg";
 
-// eslint-disable-next-line react/prop-types
-const AccordionFrame = ({ item: { heading, content } }) => {
-  const [showItem, setShowItem] = useState(false);
-  const handleClick = () => setShowItem(!showItem);
+const AccordionFrame = ({
+  item: { heading, content },
+  showItem,
+  setActiveIndex,
+}) => {
+  console.log("Micro header rendered !");
   return (
     <div className="border-[1px] rounded-md border-blue-600/[.44] ">
       <div
         className={`flex px-[36px] py-4 gap-[48px] items-center text-xl select-none cursor-pointer ${
           showItem && "border-b border-inherit"
         }`}
-        onClick={handleClick}
+        onClick={setActiveIndex}
       >
         {
           <img
-            className={showItem && "rotate-180"}
+            className={`${showItem && "rotate-180"}`}
             src={aeroDown}
             alt="Aero Down"
           />
@@ -23,9 +25,7 @@ const AccordionFrame = ({ item: { heading, content } }) => {
         <h2 className="font-medium">{heading}</h2>
       </div>
       {showItem && (
-        <p className="px-[36px] py-4 text-left transition-all ease-in duration-500 opacity-70">
-          {content}
-        </p>
+        <p className="px-[36px] py-4 text-left opacity-70">{content}</p>
       )}
     </div>
   );
